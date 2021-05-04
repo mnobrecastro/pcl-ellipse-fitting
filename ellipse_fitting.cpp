209e554f62a30c59dabe4cc88666980e8e439b1c
mnobrecastro <mnobrecastro@gmail.com>
2021-05-04 21:33:59 +0200

    Ellipse fitting real datasets (fixed).

--- ellipse_fitting.cpp
+++ ellipse_fitting.cpp
@@ -20,9 +20,10 @@
 
 using namespace std::chrono_literals;
 
-std::vector<std::array<float, 2>> ellipse_generator(std::array<float, 5>, std::array<float, 2>, int, float);
+Eigen::VectorXf fit_ellipse(std::array<Eigen::Vector3d, 6>);
 std::array<float, 5> conic2parametric(std::array<float, 6>);
 std::array<float, 6> parametric2conic(std::array<float, 5>);
+std::vector<std::array<float, 2>> ellipse_generator(std::array<float, 5>, std::array<float, 2>, int, float);
 
 int main()//(int argc, char** argv)
 {
@@ -31,9 +32,9 @@ int main()//(int argc, char** argv)
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_data(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZ>);
 
-    int N(20);
+    int N(50);
     std::array<float, 5> ellipse_par_model = { 2.0, 1.0, 1.0, 1.0, M_PI/10.0 };
-    std::vector<std::array<float, 2>> ellipse = ellipse_generator(ellipse_par_model, { 0, 2*M_PI }, N, 0.0); //0.01
+    std::vector<std::array<float, 2>> ellipse = ellipse_generator(ellipse_par_model, { 0, M_PI/3 }, N, 0.01); //0.01
 
     // PointCloud with points
     cloud_in->width = N;
@@ -48,7 +49,7 @@ int main()//(int argc, char** argv)
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////
-
+    std::cout << "A random number " << rand() % N << "/" << N << std::endl;
     /*
     // Dataset of dummy ellipse points (testing purposes)
     Eigen::Vector3d p0(1.6823, 2.1258, 0.0); 
@@ -59,12 +60,16 @@ int main()//(int argc, char** argv)
     Eigen::Vector3d p5(2.9022, 1.6246, 0.0);
     */
     // Dataset of points selected from the ellipse_generator()
-    Eigen::Vector3d p0(cloud_in->points[0].x, cloud_in->points[0].y, cloud_in->points[0].z);
-    Eigen::Vector3d p1(cloud_in->points[1].x, cloud_in->points[1].y, cloud_in->points[1].z);
-    Eigen::Vector3d p2(cloud_in->points[2].x, cloud_in->points[2].y, cloud_in->points[2].z);
-    Eigen::Vector3d p3(cloud_in->points[3].x, cloud_in->points[3].y, cloud_in->points[3].z);
-    Eigen::Vector3d p4(cloud_in->points[4].x, cloud_in->points[4].y, cloud_in->points[4].z);
-    Eigen::Vector3d p5(cloud_in->points[5].x, cloud_in->points[5].y, cloud_in->points[5].z);
+    std::array<int, 6> ridx;
+    for (size_t i(0); i < ridx.size(); ++i) {
+        ridx[i] = int(rand()%N);
+    }
+    Eigen::Vector3d p0(cloud_in->points[ridx[0]].x, cloud_in->points[ridx[0]].y, cloud_in->points[ridx[0]].z);
+    Eigen::Vector3d p1(cloud_in->points[ridx[1]].x, cloud_in->points[ridx[1]].y, cloud_in->points[ridx[1]].z);
+    Eigen::Vector3d p2(cloud_in->points[ridx[2]].x, cloud_in->points[ridx[2]].y, cloud_in->points[ridx[2]].z);
+    Eigen::Vector3d p3(cloud_in->points[ridx[3]].x, cloud_in->points[ridx[3]].y, cloud_in->points[ridx[3]].z);
+    Eigen::Vector3d p4(cloud_in->points[ridx[4]].x, cloud_in->points[ridx[4]].y, cloud_in->points[ridx[4]].z);
+    Eigen::Vector3d p5(cloud_in->points[ridx[5]].x, cloud_in->points[ridx[5]].y, cloud_in->points[ridx[5]].z);
     ////Eigen::Vector3d p6(cloud_in->points[600].x, cloud_in->points[600].y, cloud_in->points[600].z);
 
     // Dataset of six (6) point used for fitting the ellipse
@@ -121,78 +126,18 @@ int main()//(int argc, char** argv)
     //    << p4.transpose() << std::endl
     //    << p5.transpose() << std::endl;
 
-    // 2D projections only
-    Eigen::VectorXf X(6);
-    X << p0(0), p1(0), p2(0), p3(0), p4(0), p5(0);
-    Eigen::VectorXf Y(6);
-    Y << p0(1), p1(1), p2(1), p3(1), p4(1), p5(1);
 
-    // Design matrix D  
-    Eigen::MatrixXf D(6, 6);
-    D << std::pow(X(0), 2), X(0)* Y(0), std::pow(Y(0), 2), X(0), Y(0), 1.0,
-        std::pow(X(1), 2), X(1)* Y(1), std::pow(Y(1), 2), X(1), Y(1), 1.0,
-        std::pow(X(2), 2), X(2)* Y(2), std::pow(Y(2), 2), X(2), Y(2), 1.0,
-        std::pow(X(3), 2), X(3)* Y(3), std::pow(Y(3), 2), X(3), Y(3), 1.0,
-        std::pow(X(4), 2), X(4)* Y(4), std::pow(Y(4), 2), X(4), Y(4), 1.0,
-        std::pow(X(5), 2), X(5)* Y(5), std::pow(Y(5), 2), X(5), Y(5), 1.0;
-    std::cout << "**** D matrix:\n" << D << std::endl;
-
-    // Scatter matrix S
-    Eigen::MatrixXf S = Eigen::MatrixXf::Random(6, 6);
-    S = D.transpose() * D;
-    std::cout << "**** S matrix:\n" << S << std::endl;
-    //std::cout << "\t...is S self-adjoint: " << S.isApprox(S.adjoint()) << std::endl;
-
-    // Constraint matrix C
-    Eigen::MatrixXf C = Eigen::MatrixXf::Zero(6, 6);
-    C(0, 2) = -2.0;
-    C(1, 1) = 1.0;
-    C(2, 0) = -2.0;
-    std::cout << "**** C matrix:\n" << C << std::endl;
-    //Eigen::MatrixXf C_ = C.completeOrthogonalDecomposition().pseudoInverse();
-    //std::cout << "**** pinv(C) matrix:\n" << C_ << std::endl;
-
-    
-    /* ---- [ BUG FIX] ----
-     * > Compilation error may occur from using 'GeneralizedEigenSolver' due to overloading of functions in PCL and Eigen libs.
-     * + Solution: add the 'namespace pcl' to both 'aligned_{malloc,free}' in 'pcl/common/include/pcl/pcl_macros.h' (lines 379-415).
-     * This requires rebuilding the PCL lib from the source. More info can be found in the link below:
-     * https://github.com/PointCloudLibrary/pcl/issues/4734#issuecomment-830115801
-     */
-    // Solve the Generalized Eigensystem: S*a = lambda*C*a
-    Eigen::GeneralizedEigenSolver<Eigen::MatrixXf> solver;
-    solver.compute(S, C);
-    //std::cout << "The (complex) numerators of the generalized eigenvalues are: " << solver.alphas().transpose() << std::endl;
-    //std::cout << "The (real) denominators of the generalized eigenvalues are: " << solver.betas().transpose() << std::endl;
-    //std::cout << "The (complex) generalized eigenvalues are (alphas./beta): " << std::endl << solver.eigenvalues().transpose() << std::endl;
-    //std::cout << "The (complex) generalized eigenvectors are: " << std::endl << solver.eigenvectors() << std::endl;
-    Eigen::VectorXf eigvals = solver.eigenvalues().real();
-    std::cout << "Real generalized eigenvalues: " << std::endl << eigvals.transpose() << std::endl;
-    std::cout << "Real generalized eigenvectors: " << std::endl << solver.eigenvectors().real() << std::endl;
-    
-    // Find the negative eigenvalue 'neigvec'
-    int idx(0);
-    for (size_t i(0); i < eigvals.size(); ++i) {
-        if (eigvals(i) < 0 && !std::isinf(eigvals(i))) { idx = i; break; }
-    }
-    Eigen::VectorXf neigvec = solver.eigenvectors().real().col(idx);
-    neigvec /= std::abs(neigvec.maxCoeff()); // Matlab normalization (by the largest absolute value)
+    Eigen::VectorXf neigvec = fit_ellipse({p0,p1,p2,p3,p4,p5});
     std::cout << "The negative eigenvector: " << std::endl << neigvec << std::endl;
 
-    // ---- [Alternatives that do not work!] ----
-    //Eigen::GeneralizedSelfAdjointEigenSolver<Eigen::MatrixXf> solver(S, C); // MNC matrix C is singular and not PD.
-    //Eigen::EigenSolver<Eigen::MatrixXf> solver(C_*S); // Convert the problem into a single matrix, by inverting the right-hand side C matrix with a pseudo-inverse approach.
-    //std::cout << "**** Generalized Eigenvalues:\n" << solver.eigenvalues() << std::endl;
-    //std::cout << "**** Generalized Eigenvectors:\n" << solver.eigenvectors() << std::endl;
-
+    // Convert the CONIC model to a PARAMETRIC model
     std::array<float, 6> con;
     for (size_t i(0); i < neigvec.size(); ++i) { con[i] = neigvec(i); }
     std::array<float, 5> par_model_out(conic2parametric(con));
-
-    int N_out(400);
-    std::vector<std::array<float, 2>> ellipse_out = ellipse_generator(par_model_out, { 0, 2 * M_PI }, N_out, 0.0); //0.01
-
-    // populate our PointCloud with points
+ 
+    // Generate the output ellipse
+    int N_out(1000);
+    std::vector<std::array<float, 2>> ellipse_out = ellipse_generator(par_model_out, { 0, 2 * M_PI }, N_out, 0.0);
     cloud_out->width = N_out;
     cloud_out->height = 1;
     cloud_out->is_dense = false;
@@ -203,6 +148,17 @@ int main()//(int argc, char** argv)
         cloud_out->points[i].y = ellipse_out[i][1];
         cloud_out->points[i].z = 0;
     }
+
+
+    std::cout << std::endl;
+    // Print the inpu Ellipse's Parametric parameters
+    std::string parstr_in("");
+    for (auto p : ellipse_par_model) { parstr_in += std::to_string(p) + " "; }
+    std::cout << "The input PAR model: " << parstr_in << std::endl;
+    // Print the output Ellipse's Parametric parameters
+    std::string parstr_out("");
+    for (auto p : par_model_out) { parstr_out += std::to_string(p) + " "; }
+    std::cout << "The output PAR model: " << parstr_out << std::endl;
 
     //////////////////////////////////////////////////////////////////////////////////////////////////  
 
@@ -253,33 +209,70 @@ int main()//(int argc, char** argv)
 
 
 
-std::vector<std::array<float, 2>> ellipse_generator(std::array<float, 5> par, std::array<float, 2> arc = {0,2*3.1415}, int n = 100, float noise = 0.0)
+
+Eigen::VectorXf fit_ellipse(std::array<Eigen::Vector3d, 6> pts)
 {
-    /**
-     * Generates the points of an ellipse using its Parametric 'par'
-     * equation parameters, with a given arc 'ths' and noise.
+    /*
+     * 
      */
 
-    // Parametric eq.params
-    float par_a, par_b, par_h, par_k, par_t;
-    par_a = par[0]; par_b = par[1]; par_h = par[2]; par_k = par[3]; par_t = par[4];
+    // 2D projections only
+    Eigen::VectorXf X(6);
+    X << pts[0](0), pts[1](0), pts[2](0), pts[3](0), pts[4](0), pts[5](0);
+    Eigen::VectorXf Y(6);
+    Y << pts[0](1), pts[1](1), pts[2](1), pts[3](1), pts[4](1), pts[5](1);
 
-    // Generate the angle steps
-    std::vector<float> th(n, 0.0);
-    for (std::size_t i(0); i < th.size(); ++i) { th[i] = arc[0] + i * (arc[1]-arc[0])/(n); }
-    
-    // Calculate Ellipse data points
-    std::vector<std::array<float, 2>> pts(th.size(), {0.0,0.0});
-    for (std::size_t i(0); i < th.size(); ++i) {
-        pts[i][0] = par_h + std::cos(par_t) * par_a * std::cos(th[i]) - std::sin(par_t) * par_b * std::sin(th[i]);
-        pts[i][1] = par_k + std::sin(par_t) * par_a * std::cos(th[i]) + std::cos(par_t) * par_b * std::sin(th[i]);
+    // Design matrix D  
+    Eigen::MatrixXf D(6, 6);
+    D << std::pow(X(0), 2), X(0)* Y(0), std::pow(Y(0), 2), X(0), Y(0), 1.0,
+        std::pow(X(1), 2), X(1)* Y(1), std::pow(Y(1), 2), X(1), Y(1), 1.0,
+        std::pow(X(2), 2), X(2)* Y(2), std::pow(Y(2), 2), X(2), Y(2), 1.0,
+        std::pow(X(3), 2), X(3)* Y(3), std::pow(Y(3), 2), X(3), Y(3), 1.0,
+        std::pow(X(4), 2), X(4)* Y(4), std::pow(Y(4), 2), X(4), Y(4), 1.0,
+        std::pow(X(5), 2), X(5)* Y(5), std::pow(Y(5), 2), X(5), Y(5), 1.0;
+    std::cout << "**** D matrix:\n" << D << std::endl;
 
-        // Introduce noise in the data
-        pts[i][0] += rand()%100/100.0 * 2 * noise - noise;
-        pts[i][1] += rand()%100/100.0 * 2 * noise - noise;
+    // Scatter matrix S
+    Eigen::MatrixXf S = Eigen::MatrixXf::Random(6, 6);
+    S = D.transpose() * D;
+    std::cout << "**** S matrix:\n" << S << std::endl;
+
+    // Constraint matrix C
+    Eigen::MatrixXf C = Eigen::MatrixXf::Zero(6, 6);
+    C(0, 2) = -2.0;
+    C(1, 1) = 1.0;
+    C(2, 0) = -2.0;
+    std::cout << "**** C matrix:\n" << C << std::endl;
+
+    /* ---- [ BUG FIX] ----
+     * > Compilation error may occur from using 'GeneralizedEigenSolver' due to overloading of functions in PCL and Eigen libs.
+     * + Solution: add the 'namespace pcl' to both 'aligned_{malloc,free}' in 'pcl/common/include/pcl/pcl_macros.h' (lines 379-415).
+     * This requires rebuilding the PCL lib from the source. More info can be found in the link below:
+     * https://github.com/PointCloudLibrary/pcl/issues/4734#issuecomment-830115801
+     */
+     // Solve the Generalized Eigensystem: S*a = lambda*C*a
+    Eigen::GeneralizedEigenSolver<Eigen::MatrixXf> solver;
+    solver.compute(S, C);
+    Eigen::VectorXf eigvals = solver.eigenvalues().real();
+    std::cout << "Real generalized eigenvalues: " << std::endl << eigvals.transpose() << std::endl;
+    std::cout << "Real generalized eigenvectors: " << std::endl << solver.eigenvectors().real() << std::endl;
+
+    // Find the negative eigenvalue 'neigvec' (the largest, if many exist)
+    int idx(0); float absmin(0.0);
+    for (size_t i(0); i < eigvals.size(); ++i) {
+        if (eigvals(i) < absmin && !std::isinf(eigvals(i))) { idx = i; }// break;
     }
+    
+    Eigen::VectorXf neigvec = solver.eigenvectors().real().col(idx);// .normalized(); 
+    /* Matlab normalization (by the largest absolute value)
+    //if (std::abs(neigvec.maxCoeff()) > std::abs(neigvec.minCoeff())) {
+    //    neigvec /= std::abs(neigvec.maxCoeff());
+    //} else {
+    //    neigvec /= std::abs(neigvec.minCoeff());
+    //}
+    */
 
-    return pts;
+    return neigvec;
 }
 
 
@@ -308,10 +301,9 @@ std::array<float, 5> conic2parametric(std::array<float,6> con)
 
     // Calculate the eigenvalues and eigenvectors of matrix M
     Eigen::EigenSolver<Eigen::MatrixXf> solver(M);
-    std::cout << "The eigenvalues of M are:" << endl << solver.eigenvalues() << std::endl;
-    std::cout << "The matrix of eigenvectors, V, is:" << endl << solver.eigenvectors() << std::endl << std::endl;
+    std::cout << "Real eigenvalues of M are:" << endl << solver.eigenvalues().real().transpose() << std::endl;
+    //std::cout << "Real matrix of eigenvectors, V, is:" << endl << solver.eigenvectors().real() << std::endl << std::endl;
     Eigen::VectorXf eigvals = solver.eigenvalues().real();
-    std::cout << "Real eigenvalues: " << eigvals.transpose() << std::endl;
     
     // Order the eigenvalues so that | lambda_0 - con_A| <= |lambda_0 - con_C |
     if (std::abs(eigvals(0) - con_A) > std::abs(eigvals(0) - con_C)) {
@@ -319,6 +311,9 @@ std::array<float, 5> conic2parametric(std::array<float,6> con)
         eigvals(0) = eigvals(1);
         eigvals(1) = aux;
     }
+    std::cout << "Verify " << std::abs(eigvals(0) - con_A) << " <= " << std::abs(eigvals(0) - con_C) <<  std::endl;
+    std::cout << "So that " << std::abs(eigvals(1) - con_C) << " <= " << std::abs(eigvals(1) - con_A) << std::endl;
+    std::cout << "Real eigenvalues: " << eigvals.transpose() << std::endl;
     
     // Parametric eq.params
     float par_a(0.0), par_b(0.0), par_h(0.0), par_k(0.0), par_t(0.0);
@@ -327,9 +322,8 @@ std::array<float, 5> conic2parametric(std::array<float,6> con)
     par_h = (con_B * con_E - 2.0 * con_C * con_D) / (4.0 * con_A * con_C - std::pow(con_B, 2));
     par_k = (con_B * con_D - 2.0 * con_A * con_E) / (4.0 * con_A * con_C - std::pow(con_B, 2));
     par_t = (M_PI/2.0 - std::atan((con_A - con_C) / con_B)) / 2.0; // equivalent to: acot((con_A - con_C) / con_B) / 2.0;
-    std::array<float, 5> par = { par_a, par_b, par_h, par_k, par_t };
 
-    return par;
+    return { par_a, par_b, par_h, par_k, par_t };
 }
 
 std::array<float,6> parametric2conic(std::array<float, 5> par)
@@ -350,7 +344,35 @@ std::array<float,6> parametric2conic(std::array<float, 5> par)
     con_D = -2 * con_A * par_h - par_k * con_B;
     con_E = -2 * con_C * par_k - par_h * con_B;
     con_F = -std::pow(par_a * par_b, 2) + std::pow(con_A * par_h, 2) + con_B * par_h * par_k + con_C * std::pow(par_k, 2);
-    std::array<float, 6> con = { con_A, con_B, con_C, con_D, con_E, con_F };
 
-    return con;
+    return { con_A, con_B, con_C, con_D, con_E, con_F };
+}
+
+std::vector<std::array<float, 2>> ellipse_generator(std::array<float, 5> par, std::array<float, 2> arc = { 0,2 * 3.1415 }, int n = 1000, float noise = 0.0)
+{
+    /**
+     * Generates the points of an ellipse using its Parametric 'par'
+     * equation parameters, with a given arc 'ths' and noise.
+     */
+
+     // Parametric eq.params
+    float par_a, par_b, par_h, par_k, par_t;
+    par_a = par[0]; par_b = par[1]; par_h = par[2]; par_k = par[3]; par_t = par[4];
+
+    // Generate the angle steps
+    std::vector<float> th(n, 0.0);
+    for (std::size_t i(0); i < th.size(); ++i) { th[i] = arc[0] + i * (arc[1] - arc[0]) / (n); }
+
+    // Calculate Ellipse data points
+    std::vector<std::array<float, 2>> pts(th.size(), { 0.0,0.0 });
+    for (std::size_t i(0); i < th.size(); ++i) {
+        pts[i][0] = par_h + std::cos(par_t) * par_a * std::cos(th[i]) - std::sin(par_t) * par_b * std::sin(th[i]);
+        pts[i][1] = par_k + std::sin(par_t) * par_a * std::cos(th[i]) + std::cos(par_t) * par_b * std::sin(th[i]);
+
+        // Introduce noise in the data
+        pts[i][0] += std::rand() % 100 / 100.0 * 2 * noise - noise;
+        pts[i][1] += std::rand() % 100 / 100.0 * 2 * noise - noise;
+    }
+
+    return pts;
 }
--- ellipse_fitting.m
+++ ellipse_fitting.m
@@ -5,37 +5,45 @@
 % Implementation of "Direct Least Square Fitting of Ellipses" (Fitzgibbon et al., 1999)
 
 function [] = main()
+clc; clear all;
 
 % Generate dataset
-N = 6;
+N = 50;
 model = [2.0, 1.0, 1.0, 1.0, pi/10]';
-% th = linspace(0,pi/3)';
-th = linspace(2*pi/N,2*pi,N)';
-%pts = make_ellipse(model, th, 0.01);
+th = linspace(0, pi/3, N)';
+% maxang = pi/6;
+% th = linspace(maxang/N,maxang,N)';
+pts = make_ellipse(model, th, 0.001); %0.1 leads to problems
 
-% Dummy dataset
-pts = [ 1.6823, -0.2154, -0.8957, 0.3129, 2.2218, 2.9022;
-        2.1258, 1.5107, 0.3741, -0.1252, 0.4890, 1.6246;
-      ]';
-
-% Plot the dataset
-figure(1);
-axis equal;
-plot(pts(:,1), pts(:,2), '*');
+% % % Dummy dataset
+% % pts = [ 1.6823, -0.2154, -0.8957, 0.3129, 2.2218, 2.9022;
+% %         2.1258, 1.5107, 0.3741, -0.1252, 0.4890, 1.6246;
+% %       ]';
 
 % Direct Least Square Fitting of Ellipses (Fitzgibbon et al., 1999)
-con = fit_ellipse(pts(:,1), pts(:,2))
+idx = 1:N;
+idx = idx(randperm(length(idx)));
+n = 6; % Minimum number of seed points
+con = fit_ellipse( pts(idx(1:n),1), pts(idx(1:n),2));
 
 % Formulation transformation Conic -> Parametric (plot purposes)
-par = real(conic2parametric(con))
+par = real(conic2parametric(con));
 th = linspace(0,2*pi)';
 elp = make_ellipse(par, th, 0);
 
 % Plot the Ellipse model
 figure(1);
+plot(pts(:,1), pts(:,2), '*');  % Draw dataset
 hold on;
-plot(elp(:,1), elp(:,2), 'k');
+plot(pts(idx(1:n),1), pts(idx(1:n),2), 'ro');  %Draw seed pts
+hold on;
+plot(elp(:,1), elp(:,2), 'k'); % Draw Ellipse model
 hold off;
+% axis equal;
+% axis([0, 3, 0, 3]);
+
+disp(model);
+disp(par);
 
 end
 
@@ -45,21 +53,17 @@ function con = fit_ellipse(x,y)
     % This function approximates an ellipse to a set of (x,y) points.
 
     % Build design matrix D
-    D = [ x.*x, x.*y, y.*y, x, y, ones(size(x)) ];
+    D = [ x.*x, x.*y, y.*y, x, y, ones(size(x)) ]
     % Build scatter matrix S
     S = D'*D
     % Build 6x6 contraint matrix C
     C = zeros(6,6); C(1,3)=-2; C(2,2)=1; C(3,1)=-2
-    % %     C = zeros(6,6); C(1,3)=2; C(2,2)=-1; C(3,1)=2;
     % Solve generalized eigensystem: S*a = lambda*C*a
     [gevec, geval] = eig(S,C)
-    % %     [gevec, geval] = eig(pinv(S),C);
     % Find the negative eigenvalue
     [NegR, NegC] = find(geval<0 & ~isinf(geval))
-    % %     [PosR, PosC] = find(geval>0 & ~isinf(geval));
     % Get fitted Conic eq. params a
-    con = gevec(:,NegC)
-    % %     con = gevec(:,PosC);
+    con = gevec(:,NegC);
 end
 
 function par = conic2parametric(con)
@@ -89,8 +93,8 @@ function par = conic2parametric(con)
     p_b = sqrt( -det(M0) / (det(M)*eigval(2)) );
     p_h = (c_B*c_E-2*c_C*c_D) / (4*c_A*c_C-c_B^2);
     p_k = (c_B*c_D-2*c_A*c_E) / (4*c_A*c_C-c_B^2);
-    p_t = acot( (c_A-c_C)/c_B )/2;
-    % % p_t = (pi/2 - atan((c_A-c_C)/c_B))/2;
+% %     p_t = acot( (c_A-c_C)/c_B )/2;
+    p_t = (pi/2 - atan((c_A-c_C)/c_B))/2;
     par = [p_a, p_b, p_h, p_k, p_t]';
 end
 
